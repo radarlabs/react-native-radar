7856ca7875b4664f2fbed599b01c02f8d216afdb
Nick Patrick <nicholasjohnpatrick@gmail.com>
2020-03-29 19:55:41 -0400

    fix tests

--- test/index.test.js
+++ test/index.test.js
@@ -15,11 +15,21 @@ jest.mock('NativeModules', () => ({
     setMetadata: jest.fn(),
     getPermissionsStatus: jest.fn(),
     requestPermissions: jest.fn(),
+    getLocation: jest.fn(),
+    trackOnce: jest.fn(),
     startTracking: jest.fn(),
     stopTracking: jest.fn(),
-    trackOnce: jest.fn(),
     acceptEvent: jest.fn(),
     rejectEvent: jest.fn(),
+    getContext: jest.fn(),
+    searchPlaces: jest.fn(),
+    searchGeofences: jest.fn(),
+    searchPoints: jest.fn(),
+    autocomplete: jest.fn(),
+    geocode: jest.fn(),
+    reverseGeocode: jest.fn(),
+    ipGeocode: jest.fn(),
+    getDistance: jest.fn(),
   },
 }));
 
@@ -69,6 +79,30 @@ describe('calls native implementation', () => {
     expect(mockModule.requestPermissions).toBeCalledWith(background);
   });
 
+  test('getLocation', () => {
+    Radar.getLocation();
+
+    expect(mockModule.getLocation).toHaveBeenCalledTimes(1);
+  });
+
+  test('trackOnce', () => {
+    Radar.trackOnce();
+
+    expect(mockModule.trackOnce).toHaveBeenCalledTimes(1);
+  });
+
+  test('trackOnce(location)', () => {
+    const location = {
+      latitude: 40.783826,
+      longitude: -73.975363,
+      accuracy: 65,
+    };
+    Radar.trackOnce(location);
+
+    expect(mockModule.trackOnce).toHaveBeenCalledTimes(1);
+    expect(mockModule.trackOnce).toBeCalledWith(location);
+  });
+
   test('startTracking', () => {
     const options = {
       desiredStoppedUpdateInterval: 0,
@@ -99,24 +133,6 @@ describe('calls native implementation', () => {
     expect(mockModule.stopTracking).toHaveBeenCalledTimes(1);
   });
 
-  test('trackOnce', () => {
-    Radar.trackOnce();
-
-    expect(mockModule.trackOnce).toHaveBeenCalledTimes(1);
-  });
-
-  test('trackOnceWithLocation', () => {
-    const location = {
-      latitude: 40.783826,
-      longitude: -73.975363,
-      accuracy: 65,
-    };
-    Radar.trackOnce(location);
-
-    expect(mockModule.trackOnce).toHaveBeenCalledTimes(1);
-    expect(mockModule.trackOnce).toBeCalledWith(location);
-  });
-
   test('acceptEvent', () => {
     const eventId = 'eventId';
     const verifiedPlaceId = 'verifiedPlaceId';
@@ -132,6 +148,138 @@ describe('calls native implementation', () => {
 
     expect(mockModule.rejectEvent).toHaveBeenCalledTimes(1);
     expect(mockModule.rejectEvent).toBeCalledWith(eventId);
+  });
+
+  test('getContext', () => {
+    Radar.getContext();
+
+    expect(mockModule.getContext).toHaveBeenCalledTimes(1);
+  });
+
+  test('getContext(location)', () => {
+    const location = {
+      latitude: 40.783826,
+      longitude: -73.975363,
+      accuracy: 65,
+    };
+    Radar.getContext(location);
+
+    expect(mockModule.getContext).toHaveBeenCalledTimes(1);
+    expect(mockModule.getContext).toBeCalledWith(location);
+  });
+
+  test('searchPlaces', () => {
+    const options = {
+      near: {
+        latitude: 40.783826,
+        longitude: -73.975363,
+      },
+      radius: 1000,
+      chains: ['mcdonalds'],
+      limit: 10,
+    };
+    Radar.searchPlaces(options);
+
+    expect(mockModule.searchPlaces).toHaveBeenCalledTimes(1);
+    expect(mockModule.searchPlaces).toBeCalledWith(options);
+  });
+
+  test('searchGeofences', () => {
+    const options = {
+      near: {
+        latitude: 40.783826,
+        longitude: -73.975363,
+      },
+      radius: 1000,
+      tags: ['store'],
+      limit: 10,
+    };
+    Radar.searchGeofences(options);
+
+    expect(mockModule.searchGeofences).toHaveBeenCalledTimes(1);
+    expect(mockModule.searchGeofences).toBeCalledWith(options);
+  });
+
+  test('searchPoints', () => {
+    const options = {
+      near: {
+        latitude: 40.783826,
+        longitude: -73.975363,
+      },
+      radius: 1000,
+      tags: ['article'],
+      limit: 10,
+    };
+    Radar.searchPoints(options);
+
+    expect(mockModule.searchPoints).toHaveBeenCalledTimes(1);
+    expect(mockModule.searchPoints).toBeCalledWith(options);
+  });
+
+  test('autocomplete', () => {
+    const options = {
+      query: 'brooklyn roasting',
+      near: {
+        latitude: 40.783826,
+        longitude: -73.975363,
+      },
+      limit: 10,
+    };
+    Radar.searchPoints(options);
+
+    expect(mockModule.searchPoints).toHaveBeenCalledTimes(1);
+    expect(mockModule.searchPoints).toBeCalledWith(options);
+  });
+
+  test('geocode', () => {
+    const options = {
+      query: '20 jay st brooklyn',
+    };
+    Radar.geocode(options);
+
+    expect(mockModule.geocode).toHaveBeenCalledTimes(1);
+    expect(mockModule.geocode).toBeCalledWith(options);
+  });
+
+  test('reverseGeocode', () => {
+    const options = {
+      location: {
+        latitude: 40.783826,
+        longitude: -73.975363,
+      },
+    };
+    Radar.reverseGeocode(options);
+
+    expect(mockModule.reverseGeocode).toHaveBeenCalledTimes(1);
+    expect(mockModule.reverseGeocode).toBeCalledWith(options);
+  });
+
+  test('ipGeocode', () => {
+    Radar.ipGeocode();
+
+    expect(mockModule.ipGeocode).toHaveBeenCalledTimes(1);
+  });
+
+  test('getDistance', () => {
+    const options = {
+      origin: {
+        latitude: 40.78382,
+        longitude: -73.97536,
+      },
+      destination: {
+        latitude: 40.70390,
+        longitude: -73.98670,
+      },
+      modes: [
+        'foot',
+        'car',
+      ],
+      units: 'imperial',
+    };
+    Radar.getDistance(options);
+
+    expect(mockModule.getDistance).toHaveBeenCalledTimes(1);
+    expect(mockModule.getDistance).toBeCalledWith(options);
   });
 
   test('on', () => {
