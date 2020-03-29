9e2360aae19e76ba4af98c2eab66794a0ca6c9d6
Nick Patrick <nicholasjohnpatrick@gmail.com>
2020-03-29 15:47:50 -0400

    stub new APIs

--- js/index.js
+++ js/index.js
@@ -26,12 +26,8 @@ const requestPermissions = (background) => {
   NativeModules.RNRadar.requestPermissions(background);
 };
 
-const startTracking = (options) => {
-  NativeModules.RNRadar.startTracking(options);
-};
-
-const stopTracking = () => {
-  NativeModules.RNRadar.stopTracking();
+const getLocation = () => {
+  NativeModules.RNRadar.getLocation();
 };
 
 const trackOnce = (location) => {
@@ -40,6 +36,14 @@ const trackOnce = (location) => {
   } else {
     NativeModules.RNRadar.trackOnce();
   }
+};
+
+const startTracking = (options) => {
+  NativeModules.RNRadar.startTracking(options);
+};
+
+const stopTracking = () => {
+  NativeModules.RNRadar.stopTracking();
 };
 
 const acceptEvent = (eventId, verifiedPlaceId) => {
@@ -62,19 +66,65 @@ const off = (event, callback) => {
   }
 };
 
+const getContext = (location) => {
+  NativeModules.RNRadar.getContext(location);
+};
+
+const searchPlaces = (near, radius, chains, categories, groups, limit) => {
+  NativeModules.RNRadar.searchPlaces(radius, chains, categories, groups, limit, near);
+};
+
+const searchGeofences = (near, radius, tags, limit) => {
+  NativeModules.RNRadar.searchGeofences(radius, tags, limit, near);
+};
+
+const searchPoints = (near, radius, tags, limit) => {
+  NativeModules.RNRadar.searchGeofences(radius, tags, limit, near);
+};
+
+const autocomplete = (query, near, limit) => {
+  NativeModules.RNRadar.searchGeofences(query, near, limit);
+};
+
+const geocode = (address) => {
+  NativeModules.RNRadar.geocode(address);
+};
+
+const reverseGeocode = (location) => {
+  NativeModules.RNRadar.reverseGeocode(location);
+};
+
+const ipGeocode = () => {
+  NativeModules.RNRadar.ipGeocode();
+};
+
+const getDistance = (origin, destination) => {
+  NativeModules.RNRadar.getDistance(origin, destination);
+};
+
 const Radar = {
   setUserId,
   setDescription,
   setMetadata,
   getPermissionsStatus,
   requestPermissions,
+  getLocation,
+  trackOnce,
   startTracking,
   stopTracking,
-  trackOnce,
   acceptEvent,
   rejectEvent,
   on,
   off,
+  getContext,
+  searchPlaces,
+  searchGeofences,
+  searchPoints,
+  autocomplete,
+  geocode,
+  reverseGeocode,
+  ipGeocode,
+  getDistance,
 };
 
 export default Radar;
