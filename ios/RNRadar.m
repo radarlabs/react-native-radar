c5ec606fa92a47c611cd1e3d312f3d4689d66861
Brett Guenther <brett@radar.io>
2021-12-02 23:59:23 -0500

    react-native-radar  upgrade iOS SDK to 3.2.0 (#159)
    * react-native-radar-3.2-upgrade
    
    * bump to iOS 3.2.1
    
    * handle nil user in didReceiveEvents:user:
    
    Co-authored-by: Nick Patrick <nicholasjohnpatrick@gmail.com>

--- ios/RNRadar.m
+++ ios/RNRadar.m
@@ -49,13 +49,14 @@ RCT_EXPORT_MODULE();
     hasListeners = NO;
 }
 
-- (void)didReceiveEvents:(NSArray<RadarEvent *> *)events user:(RadarUser *)user {
+- (void)didReceiveEvents:(NSArray<RadarEvent *> *)events user:(RadarUser * _Nullable )user {
     if (hasListeners) {
-        [self sendEventWithName:@"events" body:@{
-            @"events": [RadarEvent arrayForEvents:events],
-            @"user": [user dictionaryValue]
-
-        }];
+        NSMutableDictionary *body = [NSMutableDictionary new];
+        [body setValue:[RadarEvent arrayForEvents:events] forKey:@"events"];
+        if (user) {
+            [body setValue:[user dictionaryValue] forKey:@"user"];
+        }
+        [self sendEventWithName:@"events" body:body];
     }
 }
 
@@ -73,7 +74,7 @@ RCT_EXPORT_MODULE();
         [self sendEventWithName:@"clientLocation" body:@{
             @"location": [Radar dictionaryForLocation:location],
             @"stopped": @(stopped),
-            @"source": [Radar stringForSource:source]
+            @"source": [Radar stringForLocationSource:source]
         }];
     }
 }
@@ -245,15 +246,15 @@ RCT_EXPORT_METHOD(trackOnce:(NSDictionary *)locationDict resolve:(RCTPromiseReso
 }
 
 RCT_EXPORT_METHOD(startTrackingEfficient) {
-    [Radar startTrackingWithOptions:RadarTrackingOptions.efficient];
+    [Radar startTrackingWithOptions:RadarTrackingOptions.presetEfficient];
 }
 
 RCT_EXPORT_METHOD(startTrackingResponsive) {
-    [Radar startTrackingWithOptions:RadarTrackingOptions.responsive];
+    [Radar startTrackingWithOptions:RadarTrackingOptions.presetResponsive];
 }
 
 RCT_EXPORT_METHOD(startTrackingContinuous) {
-    [Radar startTrackingWithOptions:RadarTrackingOptions.continuous];
+    [Radar startTrackingWithOptions:RadarTrackingOptions.presetContinuous];
 }
 
 RCT_EXPORT_METHOD(startTrackingCustom:(NSDictionary *)optionsDict) {
