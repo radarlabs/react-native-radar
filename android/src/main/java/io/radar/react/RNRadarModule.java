90bfad17d38ec6435ed844d90b0fdd6c7fd3c946
David Goodfellow <61531269+david-goodfellow@users.noreply.github.com>
2023-03-13 16:48:33 -0400

    LogConversion without name

--- android/src/main/java/io/radar/react/RNRadarModule.java
+++ android/src/main/java/io/radar/react/RNRadarModule.java
@@ -1099,7 +1099,7 @@ public class RNRadarModule extends ReactContextBaseJavaModule implements Permiss
         }
         
         JSONObject metadataObj = RNRadarUtils.jsonForMap(metadata);
-        Radar.logConversionWithName(name, metadataObj, new Radar.RadarLogConversionCallback() {
+        Radar.logConversion(name, metadataObj, new Radar.RadarLogConversionCallback() {
             @Override
             public void onComplete(@NonNull Radar.RadarStatus status, @Nullable RadarEvent event) {
                 try {
