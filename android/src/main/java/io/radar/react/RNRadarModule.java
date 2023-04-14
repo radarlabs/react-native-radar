4d547362f228d3897c4ab8160fd222ff1c47a140
Liam Meier <liam.meier@radar.com>
2023-04-14 17:59:44 -0400

    Revert "more logging"
    This reverts commit 0b199e89f8df3b812ac30ccf24933de777c6ff77.

--- android/src/main/java/io/radar/react/RNRadarModule.java
+++ android/src/main/java/io/radar/react/RNRadarModule.java
@@ -329,8 +329,6 @@ public class RNRadarModule extends ReactContextBaseJavaModule implements Permiss
                 if (promise == null) {
                     return;
                 }
-                Log.e("trackVerified", "trackVerified");
-                Log.e(TAG, "trackVerified: " + status.toString() + ", " + location + ", " + events + ", " + user);
 
                 try {
                     if (status == Radar.RadarStatus.SUCCESS) {
