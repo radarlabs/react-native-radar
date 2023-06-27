c25f1b6f1f6fc565f3c199037ee5c2fd31056378
Liam Meier <liam.meier@radar.com>
2023-06-27 16:50:44 -0400

    Map, minimal configuration

--- js/ui/map.jsx
+++ js/ui/map.jsx
@@ -1,14 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View, Image } from 'react-native';
 import MapLibreGL from '@maplibre/maplibre-react-native';
 import { getHost, getPublishableKey } from '../helpers';
-import Radar from '../index.native';
 
 const DEFAULT_STYLE = 'radar-default-v1';
-
-const defaultMarkerOptions = {
-  color: '#000257',
-};
 
 const styles = StyleSheet.create({
   container: {
@@ -19,29 +14,11 @@ const styles = StyleSheet.create({
   },
   logo: {
     position: 'absolute',
-    bottom: -15,
+    bottom: -10,
     left: 5,
     width: 50,
     height: 50,
     resizeMode: 'contain',
-  },
-  userLocation: {
-    pulse: {
-      circleRadius: 15,
-      circleColor: '#000257',
-      circleOpacity: 0.2,
-      circlePitchAlignment: 'map',
-    },
-    background: {
-      circleRadius: 9,
-      circleColor: '#fff',
-      circlePitchAlignment: 'map',
-    },
-    foreground: {
-      circleRadius: 6,
-      circleColor: '#000257',
-      circlePitchAlignment: 'map',
-    },
   },
 });
 
@@ -51,67 +28,11 @@ const createStyleURL = async (style = DEFAULT_STYLE) => {
   return `${host}/maps/styles/${style}?publishableKey=${publishableKey}`;
 };
 
-const RadarMap = ({ mapOptions, markerOptions }) => {
-  const [userLocation, setUserLocation] = useState(null);
-
+const RadarMap = ({ mapOptions, children }) => {
   const [styleURL, setStyleURL] = useState(null);
   createStyleURL(mapOptions?.mapStyle || DEFAULT_STYLE).then((result) => {
     setStyleURL(result);
   });
-
-
-  const [cameraConfig, setCameraConfig] = useState({
-    triggerKey: Date.now(),
-    centerCoordinate: [-94.630146, 39.113581],
-    animationMode: 'flyTo',
-    animationDuration: 600,
-    zoomLevel: 2, // was 12
-  });
-
-  const geoJSONUserLocation = {
-    type: 'FeatureCollection',
-    features: (userLocation !== null) ? [
-      {
-        type: 'Feature',
-        geometry: {
-          type: 'Point',
-          coordinates: [userLocation.longitude, userLocation.latitude],
-        },
-      },
-    ] : [],
-  };
-
-  const userLocationMapIndicator = (
-    <MapLibreGL.ShapeSource
-      id="user-location"
-      shape={geoJSONUserLocation}
-    >
-      <MapLibreGL.CircleLayer
-        id="user-location-inner"
-        style={styles.userLocation.pulse}
-      />
-      <MapLibreGL.CircleLayer
-        id="user-location-middle"
-        style={styles.userLocation.background}
-      />
-      <MapLibreGL.CircleLayer
-        id="user-location-outer"
-        style={styles.userLocation.foreground}
-      />
-
-    </MapLibreGL.ShapeSource>
-  );
-
-  useEffect(() => {
-    Radar.getLocation().then((result) => {
-      if (result.status === 'SUCCESS') {
-        setUserLocation({
-          latitude: result.location.latitude,
-          longitude: result.location.longitude,
-        });
-      }
-    });
-  }, []);
 
   if (!styleURL) {
     return null;
@@ -124,23 +45,12 @@ const RadarMap = ({ mapOptions, markerOptions }) => {
         pitchEnabled={false}
         compassEnabled={false}
         logoEnabled={false}
-        attributionEnabled={false}
-        // onRegionDidChange={mapOptions.onRegionDidChange}
+        attributionEnabled
+        onRegionDidChange={mapOptions.onRegionDidChange ? mapOptions.onRegionDidChange : null}
         styleURL={styleURL}
       >
-        {userLocationMapIndicator}
-        <MapLibreGL.Camera
-          {...cameraConfig}
-        />
-        {markerOptions && (
-          <MapLibreGL.PointAnnotation
-            id="marker"
-            {...defaultMarkerOptions}
-            {...markerOptions}
-          >
-            <MapLibreGL.Callout title={markerOptions.text} />
-          </MapLibreGL.PointAnnotation>
-        )}
+        <MapLibreGL.UserLocation />
+        {children}
       </MapLibreGL.MapView>
       <Image
         source={require('./map-logo.png')}
