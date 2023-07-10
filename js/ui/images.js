6e64da83e27fc98bbd1d00ad69941be6a4ac12f9
Liam Meier <liam.meier@radar.com>
2023-07-10 11:53:17 -0400

    Clean up file

---
+++ js/ui/images.js
@@ -0,0 +1,4 @@
+export const BACK_ICON = require('./back.png');
+export const SEARCH_ICON = require('./search.png');
+export const RADAR_LOGO = require('./radar-logo.png');
+export const MARKER_ICON = require('./marker.png');
