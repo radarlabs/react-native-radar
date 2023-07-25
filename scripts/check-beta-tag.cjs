57d2ab4c223a936bcaa4c489805ef0ae62e4494a
Liam Meier <liam.meier@radar.com>
2023-07-25 14:58:20 -0600

    Fix github release process, distinguishing between release and pre release

---
+++ scripts/check-beta-tag.cjs
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const packageJSON = require('../package.json');
+const packageLockJSON = require('../package-lock.json');
+
+let tagVersion = process.env.GITHUB_REF_NAME || '';
+console.log('Checking tag:', tagVersion);
+
+
+if (!tagVersion.includes('-beta')) {
+  console.error('Prelease version should contiain a "-beta" suffix');
+  process.exit(1);
+}
+
+if (tagVersion !== packageJSON.version) {
+  console.error('VERSION MISMATCH - version does not match package.json.');
+  process.exit(1);
+}
+if (tagVersion !== packageLockJSON.version) {
+  console.error('VERSION MISMATCH - version does not match package-lock.json.');
+  process.exit(1);
+}
+console.log('Tag OK.', tagVersion);
