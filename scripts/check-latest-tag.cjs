be23434ba731dda7015e25b7fc3a6364ff1b311d
Liam Meier <liam.meier@radar.com>
2023-08-02 13:31:11 -0600

    Fix github release process (#238)
    * Fix github release process, distinguishing between release and pre release
    
    * Name each github workflow differently
    
    * Update scripts, package.json
    
    * Remove v check
    
    * Remove force
    
    * Bump to 3.8.3

---
+++ scripts/check-latest-tag.cjs
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const packageJSON = require('../package.json');
+const packageLockJSON = require('../package-lock.json');
+
+let tagVersion = process.env.GITHUB_REF_NAME || '';
+console.log('Checking tag:', tagVersion);
+
+
+if (tagVersion.includes('-')) {
+  console.error('Latest tag should not include any suffix:', `-${tagVersion.split('-')[1]}`);
+  process.exit(1);
+}
+
+if (tagVersion !== packageJSON.version) {
+  console.error('VERSION MISMATCH - version does not match package.json.');
+  process.exit(1);
+}
+if (tagVersion !== packageLockJSON.version) {
+  console.error('VERSION MISMATCH - version does not match package-lock.json.');
+  process.exit(1);
+}
+console.log('Tag OK.', tagVersion);
