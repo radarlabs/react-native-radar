ef95ef6b1bada4b7414c7dab215d8b996b10fa71
Liam Meier <liam.meier@radar.com>
2023-09-12 11:56:38 -0400

    wrap getLocation in a try due to possible permissions error

--- js/ui/map.jsx
+++ js/ui/map.jsx
@@ -31,10 +31,16 @@ const RadarMap = ({ mapOptions, children }) => {
 
   useEffect(() => {
     Radar.getLocation().then((result) => {
+      if (!result.location || !result.location.latitude || !result.location.longitude) {
+        return;
+      }
       setUserLocation({
         latitude: result.location.latitude,
         longitude: result.location.longitude,
       });
+    }).catch((err) => {
+      // eslint-disable-next-line no-console
+      console.warn(`Radar SDK: Failed to get location: ${err}`);
     });
   }, [mapOptions]);
 
