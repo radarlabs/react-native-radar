676e3370265cdb52c5423569f7ae9b2ba79fe7b9
Kenny Hu <kenny.hu@radar.com>
2024-01-25 16:16:19 -0500

    build on expo but with runtime errors

--- example/ios/Example/AppDelegate.h
+++ example/ios/Example/AppDelegate.h
@@ -1,11 +1,8 @@
-#import <Foundation/Foundation.h>
-#import <React/RCTBridgeDelegate.h>
+#import <RCTAppDelegate.h>
 #import <UIKit/UIKit.h>
-
 #import <Expo/Expo.h>
 
 @interface AppDelegate : EXAppDelegateWrapper
 
-@property (nonatomic, strong) UIWindow *window;
-
 @end
+
