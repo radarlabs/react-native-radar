0f38b3eed77d4cd905c89281b03bcc0fe78cfa0d
Kenny Hu <kenny.hu@radar.com>
2024-01-26 12:53:23 -0500

    add declarations in dist

--- dist/src/index.d.ts
+++ dist/src/index.d.ts
@@ -1,18 +1,9 @@
 import { Platform } from "react-native";
 import { RadarNativeInterface } from "./@types/RadarNativeInterface";
-
 type RadarInterface = Platform["OS"] extends "web" ? any : RadarNativeInterface;
-
-let module: RadarInterface;
-if (Platform.OS === "web") {
-  module = require("./index.web").default;
-} else {
-  module = require("./index.native").default;
-}
+declare let module: RadarInterface;
 export default module;
-
 export { default as Autocomplete } from "./ui/autocomplete";
 export { default as Map } from "./ui/map";
-
 export * from "./@types/types";
 export * from "./@types/RadarNativeInterface";
---
+++ dist/src/index.js
@@ -0,0 +1,35 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Map = exports.Autocomplete = void 0;
+const react_native_1 = require("react-native");
+let module;
+if (react_native_1.Platform.OS === "web") {
+    module = require("./index.web").default;
+}
+else {
+    module = require("./index.native").default;
+}
+exports.default = module;
+var autocomplete_1 = require("./ui/autocomplete");
+Object.defineProperty(exports, "Autocomplete", { enumerable: true, get: function () { return __importDefault(autocomplete_1).default; } });
+var map_1 = require("./ui/map");
+Object.defineProperty(exports, "Map", { enumerable: true, get: function () { return __importDefault(map_1).default; } });
+__exportStar(require("./@types/types"), exports);
+__exportStar(require("./@types/RadarNativeInterface"), exports);
