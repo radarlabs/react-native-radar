f9eeec010966c1170ce4937866b4217a9a3219eb
Kenny Hu <kenny.hu@radar.com>
2024-01-29 11:44:49 -0500

    change index

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,21 @@
-import { Platform } from "react-native";
+//import { Platform } from "react-native";
 import { RadarNativeInterface } from "./@types/RadarNativeInterface";
 
-type RadarInterface = Platform["OS"] extends "web" ? any : RadarNativeInterface;
+//type RadarInterface = Platform["OS"] extends "web" ? RadarNativeInterface : any;
+//type RadarInterface = RadarNativeInterface;
 
-let module: RadarInterface;
-if (Platform.OS === "web") {
-  module = require("./index.web").default;
-} else {
+let module: RadarNativeInterface;
+// if (Platform.OS === "web") {
+//   module = require("./index.web").default;
+// } else {
   module = require("./index.native").default;
-}
+//}
 export default module;
 
+export { default as RadarRNWeb } from "./index.web";
 export { default as Autocomplete } from "./ui/autocomplete";
 export { default as Map } from "./ui/map";
+
 
 export * from "./@types/types";
 export * from "./@types/RadarNativeInterface";
