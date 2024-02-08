874feffccff0468a337a9b848ac836d57e684295
York Wei <york.wei@icloud.com>
2024-02-08 14:23:05 -0500

    for build

---
+++ dist/src/ui/images.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CLOSE_ICON = exports.MARKER_ICON = exports.RADAR_LOGO = exports.SEARCH_ICON = exports.BACK_ICON = void 0;
+exports.BACK_ICON = require('./back.png');
+exports.SEARCH_ICON = require('./search.png');
+exports.RADAR_LOGO = require('./radar-logo.png');
+exports.MARKER_ICON = require('./marker.png');
+exports.CLOSE_ICON = require('./close.png');
---
+++ dist/src/ui/styles.d.ts
@@ -0,0 +1,2 @@
+export default styles;
+declare const styles: any;
