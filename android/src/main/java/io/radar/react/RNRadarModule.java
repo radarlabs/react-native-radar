de31ae7983b892ccc860a5dd39ce270c199f95d6
York Wei <york.wei@icloud.com>
2024-02-14 17:33:47 -0500

    Update RNRadarModule.java

--- android/src/main/java/io/radar/react/RNRadarModule.java
+++ android/src/main/java/io/radar/react/RNRadarModule.java
@@ -2,6 +2,8 @@ package io.radar.react;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Build;
