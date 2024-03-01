7cbd77b0533dda29c059ce6b5dd486cfe455ba70
Kenny Hu <kenny.hu@radar.com>
2024-03-01 11:39:53 -0500

    save for branch change

---
+++ withRadar.ts
@@ -0,0 +1,29 @@
+import { ConfigPlugin, withInfoPlist } from 'expo/config-plugins';
+
+// Pass `<string>` to specify that this plugin requires a string property.
+const withRadar: ConfigPlugin<string> = (config) => {
+  return withInfoPlist(config, config => {
+    config.modResults.NSLocationWhenInUseUsageDescription = 'This app uses the location service to provide location-based services.';
+    config.modResults.NSLocationAlwaysAndWhenInUseUsageDescription = 'This app uses the location service to provide location-based services.';
+    config.modResults.UIBackgroundModes = ['location', 'fetch'];
+    config.modResults.NSAppTransportSecurity = {
+      "NSAllowsArbitraryLoads": false,
+      "NSPinnedDomains": {
+        "api-verified.radar.io": {
+          "NSIncludesSubdomains": true,
+          "NSPinnedLeafIdentities": [
+            {
+              "SPKI-SHA256-BASE64": "15ktYXSSU2llpy7YyCgeqUKDBkjcimK/weUcec960sI="
+            },
+            {
+              "SPKI-SHA256-BASE64": "15ktYXSSU2llpy7YyCgeqUKDBkjcimK/weUcec960sI="
+            }
+          ]
+        }
+      }
+    }
+    return config;
+  });
+};
+
+module.exports.withRadar = withRadar;
