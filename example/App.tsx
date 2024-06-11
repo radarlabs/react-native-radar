218090e86834bd994cbdafc5ba3435b32245fd79
ShiCheng Lu <shicheng.lu@radar.com>
2024-06-11 13:43:00 -0400

    update maplibre

--- example/App.tsx
+++ example/App.tsx
@@ -35,7 +35,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={{ width: "100%", height: "40%" }}>
-        {/* <Map /> */}
+        <Map />
       </View>
       <View style={{ width: "100%", height: "10%" }}>
         {/* <Autocomplete
