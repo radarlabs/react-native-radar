914aa4875f3538621365cf562c2b0c99bc0c9167
ShiCheng Lu <shicheng.lu@radar.com>
2024-07-31 10:05:04 -0400

    implement validateAddress

--- ios/RNRadar.m
+++ ios/RNRadar.m
@@ -960,6 +960,27 @@ RCT_EXPORT_METHOD(ipGeocode:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRe
     }];
 }
 
+RCT_EXPORT_METHOD(validateAddress:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject) {
+    __block RCTPromiseResolveBlock resolver = resolve;
+    __block RCTPromiseRejectBlock rejecter = reject;
+
+    [Radar validateAddress:^(RadarStatus status, RadarAddress * _Nullable address, RadarAddressVerificationStatus verificationStatus) {
+        if (status == RadarStatusSuccess && resolver) {
+            NSMutableDictionary *dict = [NSMutableDictionary new];
+            [dict setObject:[Radar stringForStatus:status] forKey:@"status"];
+            if (address) {
+                [dict setObject:[address dictionaryValue] forKey:@"address"];
+            }
+            dict[@"verificationStatus"] = [Radar stringForVerificationStatus: verificationStatus];
+            resolver(dict);
+        } else if (rejecter) {
+            rejecter([Radar stringForStatus:status], [Radar stringForStatus:status], nil);
+        }
+        resolver = nil;
+        rejecter = nil;
+    }];
+}
+
 RCT_EXPORT_METHOD(getDistance:(NSDictionary *)optionsDict resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject) {
     if (optionsDict == nil) {
         if (reject) {
