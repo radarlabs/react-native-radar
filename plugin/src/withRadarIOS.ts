69103feec68b182fd29f8061499d3b1561edf3ee
Liam Meier <liam.meier@radar.com>
2024-11-08 17:28:54 -0500

    bump version

--- plugin/src/withRadarIOS.ts
+++ plugin/src/withRadarIOS.ts
@@ -52,8 +52,7 @@ export const withRadarIOS: ConfigPlugin<RadarPluginProps> = (config, args) => {
         const contents = await fs.readFile(filePath, 'utf-8');
 
         // Check if the pod declaration already exists
-        const RadarSDKMotionVersion = '3.18.3'
-        if (contents.indexOf(`pod 'RadarSDKMotion', ${RadarSDKMotionVersion}`) === -1) {
+        if (contents.indexOf("pod 'RadarSDKMotion', '3.18.5-beta.1'") === -1) {
           // Find the target block
           const targetRegex = /target '(\w+)' do/g;
           const match = targetRegex.exec(contents);
@@ -63,7 +62,7 @@ export const withRadarIOS: ConfigPlugin<RadarPluginProps> = (config, args) => {
 
             // Insert the pod declaration within the target block
             const targetBlock = contents.substring(targetStartIndex, targetEndIndex);
-            const updatedTargetBlock = targetBlock.replace(/(target '(\w+)' do)/, `$1\n  pod 'RadarSDKMotion', '3.18.3'`);
+            const updatedTargetBlock = targetBlock.replace(/(target '(\w+)' do)/, `$1\n  pod 'RadarSDKMotion', '3.18.5-beta.1'`);
             const newContents = contents.replace(targetBlock, updatedTargetBlock);
 
             // Write the updated contents back to the Podfile
