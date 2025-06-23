54e452e29ec4fa5f7aab3e016418ce349f348043
Kenny Hu <kenny.hu@radar.com>
2025-06-23 15:19:34 -0400

    .

--- app.plugin.js
+++ app.plugin.js
@@ -1,1 +1,5 @@
-module.exports = require('./plugin/build');
+// Handle ES module import for the plugin
+const plugin = require('./plugin/build');
+
+// Export the plugin function that handles ES module imports
+module.exports = plugin.default;
