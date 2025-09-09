11c2cb8efaa791f65e3bf8b75acca93122a648bb
Kenny Hu <kenny.hu@radar.com>
2025-09-09 14:48:12 -0400

    fix merge

--- android/src/newarch/java/com/radar/RadarModule.kt
+++ android/src/newarch/java/com/radar/RadarModule.kt
@@ -146,8 +146,8 @@ class RadarModule(reactContext: ReactApplicationContext) :
         editor.putString("x_platform_sdk_version", "3.23.1")
         editor.apply()
 
-        Radar.initialize(reactApplicationContext, publishableKey, radarInAppMessageReceiver, Radar.RadarLocationServicesProvider.GOOGLE, fraud, null, null, currentActivity)
-        
+        Radar.initialize(reactApplicationContext, publishableKey, null, Radar.RadarLocationServicesProvider.GOOGLE, fraud, null, null, currentActivity)
+        Radar.setInAppMessageReceiver(radarInAppMessageReceiver)
         if (fraud) {
             Radar.setVerifiedReceiver(radarVerifiedReceiver)
         } 
